21e405bdda0d19c824c0ed1d599a4ffd46822fa7
Pkt-Rkt <danielchiscock@gmail.com>
2023-10-21 13:10:07 +1300

    Lab1

---
+++ Lab1/Lab1-Vite/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Exercise 1</h1>
+      <Greeting name="Mirza">Welcome to my React!</Greeting>
+    </div>
+  );
+}
+
+export default App;
